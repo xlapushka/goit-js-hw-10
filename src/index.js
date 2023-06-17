11cb141a97012941a1f2433b42fc8a506356ab05
Yuliia Hubarenko <xlapushka@ukr.net>
2023-06-17 16:48:32 +0300

    task 2 is ready

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {fetchBreeds} from './js/cat-api'
+import {fetchBreeds, fetchCatByBreed, fetchCatInfo } from './js/cat-api'
 
 const refs = {
   loader : document.querySelector(".loader"),
@@ -6,7 +6,6 @@ const refs = {
   catInfo : document.querySelector(".cat-info"),
   error : document.querySelector(".error"),
 }
-
 
 let currentBreed = '';
 
@@ -36,33 +35,72 @@ function createMarkUpSelect(arr) {
 }
 
 
-
 // =====================cat's info=======================
-
 
 function chosenBreed(evt) {
   evt.preventDefault();
   currentBreed = evt.target.value;
+  fetchCatByBreed(currentBreed).then(data => refs.catInfo.innerHTML = createMarkUpBreedImg(data)).catch(err => console.log(err));
 };
 
-// function getSomething(limit, breedId) {
-//   return fetch(`${BASE_URL}?key=${API_KEY}&limit=${limit}&page=${breedId}`).then(resp => 
-//     { 
-//       if (!resp.ok) {
-//         throw new Error("resp.statusText");
-//       };
-//     return resp.json()
-//     })
-// };
+function createMarkUpBreedImg(data) {
+  let id = data[0].id;  
+  fetchCatInfo(id).then(data => refs.catInfo.insertAdjacentHTML('beforeend', createMarkUpBreedInfo(data))).catch(err => console.log(err));
 
-// function createMarkUpInfo() {
-//   return arr.{name, description, temperament} => `
-//       <img src="" class="cat's img" alt="cat" width="200px">
-//       <div>
-//         <h1 class="cat's breed">${name}</h1> 
-//         <p class="breed's description">${description}</p>
-//         <p class="temperament's description">
-//           <span>Temperament: </span>${temperament}</p>
-//       </div>  `.join('')
+  return `<img src="${data[0].url}" class="cat's img" alt="cat" width="300px">
+`};
+
+function createMarkUpBreedInfo(data) {
+  let breed = data.breeds[0];
+  return ` <div>
+            <h1 class="cat's breed">${breed.name}</h1> 
+            <p class="breed's description">${breed.description}</p>
+            <p class="temperament's description">
+              <span class="span">Temperament: </span>
+              ${breed.temperament}
+            </p>
+          </div> `
+};
+
+
+
+
+
+
+
+// function createMarkUpBreedInfo(data) {
+
+//   let id = data[0].id;
+//   console.log(getInfo(id));
+//   let {catsBreed,
+//       breedsDescription,
+//       temperamentDescription} = getInfo(id);
+
+
+//   // fetchCatInfo(id).then(data => console.log(data)).catch(err => console.log(err));
+  
+//   return `<img src="${data[0].url}" class="cat's img" alt="cat" width="300px">
+// `};
+  
+
+// function getInfo(id) {
+
+//   fetchCatInfo(id).then(data => {console.log(data.breeds[0].name)
+//     // let catsBreed = http.name;
+//     // let breedsDescription = http.description;
+//     // let temperamentDescription = http.temperament;
+//   }).catch(err => console.log(err));
+
+  
+//   // console.log(data.breeds[0].name, data.breeds[0].description, data.breeds[0].temperament)
+//   // data.breeds[0].name
+//   return {
+//     catsBreed : data.breeds[0].name,
+//     breedsDescription : data.breeds[0].description,
+//     temperamentDescription : data.breeds[0].temperament
+//   } 
 // }
+
+
+
 
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,19 +1,12 @@
 
-const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+const BASE_BREEDS_URL = 'https://api.thecatapi.com/v1/breeds';
+const BASE_IMG_URL = 'https://api.thecatapi.com/v1/images/search';
+const BASE_INFO_URL = 'https://api.thecatapi.com/v1/images'
 const API_KEY = 'live_wElV4PUyrnCsA8lLHCrkJGkYmZocPinp5ZbBGBclEtZAVXWBWGCcrwJQqD8p3U9O';
 
-// function fetchBreeds() {
-//   return fetch(`${BASE_URL}?key=${API_KEY}&l`).then(resp => 
-//     { 
-//       if (!resp.ok) {
-//         throw new Error("resp.statusText");
-//       };
-//     return resp.json().then(data => breedSelect.innerHTML = createMarkUpSelect(data)).catch(err => console.log(err));
-//     })
-// };
 
-export function fetchBreeds() {
-  return fetch(`${BASE_URL}?key=${API_KEY}&l`).then(resp => 
+function fetchBreeds() {
+  return fetch(`${BASE_BREEDS_URL}?key=${API_KEY}&l`).then(resp => 
     { 
       if (!resp.ok) {
         throw new Error("resp.statusText");
@@ -21,3 +14,27 @@ export function fetchBreeds() {
     return resp.json()
     })
 };
+
+
+function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_IMG_URL}?key=${API_KEY}&breed_ids=${breedId}`).then(resp => 
+    { 
+      if (!resp.ok) {
+        throw new Error("resp.statusText");
+      };
+    return resp.json()
+    })
+};
+
+
+function fetchCatInfo(id) {
+  return fetch(`${BASE_INFO_URL}/${id}`).then(resp => 
+    { 
+      if (!resp.ok) {
+        throw new Error("resp.statusText");
+      };
+    return resp.json()
+    })
+};
+
+export { fetchBreeds, fetchCatByBreed, fetchCatInfo }
